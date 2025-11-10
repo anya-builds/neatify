9b62b26811fc99943401c5ca0f6982742a84d545
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-11-11 00:18:21 +0530

    waste management aapp

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,4 +2,5 @@
 import { useState, useEffect } from "react"
 import Link from "next/link"
 import { usePathname } from 'next/navigation'
-import { Button } from "@/components/ui/button"
+import { Button } from "./ui/button"
+import { Menu, Coins, Leaf, Search, Bell, User, ChevronDown, LogIn, LogOut } from "lucide-react"
