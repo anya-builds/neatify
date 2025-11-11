e8593fc35c23923b76369572e6ae0df5267fd9b9
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-11-11 19:16:53 +0530

    waste management aapp

--- components/Header.tsx
+++ components/Header.tsx
@@ -15,6 +15,7 @@ import { Web3Auth } from "@web3auth/modal"
 import { CHAIN_NAMESPACES, IProvider, WEB3AUTH_NETWORK } from "@web3auth/base"
 import { EthereumPrivateKeyProvider } from "@web3auth/ethereum-provider"
 import { createUser, getUnreadNotifications, markNotificationAsRead, getUserByEmail, getUserBalance } from "@/utils/db/actions"
+import { useMediaQuery } from "@/hooks/useMediaQuery"
 
 const clientId = process.env.WEB3_AUTH_CLIENT_ID
 
