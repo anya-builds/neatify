389ec2cc03281329c4275dc33f5a459a2c65906b
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-11-11 17:09:47 +0530

    waste management aapp

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,3 +4,9 @@ import Link from "next/link"
 import { usePathname } from 'next/navigation'
 import { Button } from "./ui/button"
 import { Menu, Coins, Leaf, Search, Bell, User, ChevronDown, LogIn, LogOut } from "lucide-react"
+import { 
+  DropdownMenu, 
+  DropdownMenuContent, 
+  DropdownMenuItem, 
+  DropdownMenuTrigger 
+} from "@/components/ui/dropdown-menu"
