96b58e0b4efa99c64373538c2d8ce48bb37dc3ab
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-11-16 22:47:11 +0530

    neatify

--- utils/db/dbConfig.jsx
+++ utils/db/dbConfig.jsx
@@ -1,8 +1,8 @@
-// postgresql://neondb_owner:npg_nrVEO4W0isFM@ep-falling-waterfall-ahd7aqkq-pooler.c-3.us-east-1.aws.neon.tech/neatify-database?sslmode=require&channel_binding=require
-import { neon } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-http";
-import * as schema from "./schema";
-const sql = neon(
-  process.env.DATABASE_URL
-);
-export const db = drizzle(sql, { schema });
+// // postgresql://neondb_owner:npg_nrVEO4W0isFM@ep-falling-waterfall-ahd7aqkq-pooler.c-3.us-east-1.aws.neon.tech/neatify-database?sslmode=require&channel_binding=require
+// import { neon } from "@neondatabase/serverless";
+// import { drizzle } from "drizzle-orm/neon-http";
+// import * as schema from "./schema";
+// const sql = neon(
+//   process.env.DATABASE_URL
+// );
+// export const db = drizzle(sql, { schema });
